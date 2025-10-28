50b5c55b3ee9163d61221f17b86feed150abf537
Leonardo Amaral Alves <leonardoamaralalves1@gmail.com>
2025-10-28 18:36:16 -0300

    gab

---
+++ index.js
@@ -0,0 +1,11 @@
+
+i = 0
+let vet = []
+do{
+    let msg = Number(prompt('digite os numeros. (digite 0 para encerrar)'))
+}
+while (msg !==0) {
+        msg = Number(prompt('digite os numeros. (digite 0 para encerrar)'))
+         vet[i] = msg
+         i++
+}
