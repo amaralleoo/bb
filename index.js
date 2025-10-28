b7a13a0454dde3e59c970a76bd48080133149b3c
Leonardo Amaral Alves <leonardoamaralalves1@gmail.com>
2025-10-28 19:06:30 -0300

    final

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-
 i = 0
 let vet = []
 do{
-    let msg = Number(prompt('digite os numeros. (digite 0 para encerrar)'))
-}
-while (msg !==0) {
-        msg = Number(prompt('digite os numeros. (digite 0 para encerrar)'))
+        msg = Number(prompt('digite  numeros. (digite 0 para encerrar)'))
          vet[i] = msg
          i++
-}
+}while (msg !==0)
+    vet.pop()    
+
+let par = []
+let impar = []
+let j = 0
+let k = 0
+
+for (i = 0; i < vet.length; i++){
+     if (vet[i] % 2 == 0){
+    par [j] = vet[i]
+        j++
+    }
+    else if(vet[i] % 2 !== 0){
+        impar[k] = vet[i]
+        k++
+    }
+} 
+console.log(`numeros pares ${par}`)
+console.log(`numeros impares ${impar}`)
